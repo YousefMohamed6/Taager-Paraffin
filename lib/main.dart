5826877c2ac6722bcd6c391aa56bba2314b9f3f9
Yousef Mohamed flutter <yousef.mohamed.flutter@appyinnovator.com>
2025-08-15 16:22:50 +0300

    Update pubspec.yaml to align SDK and dependency versions
    - Updated Dart SDK constraint from '>=3.2.6 <4.0.0' to '>=3.3.0 <4.0.0'.
    - Downgraded flutter_local_notifications from ^19.4.0 to ^19.1.0.
    - Downgraded flutter_localization from ^0.3.3 to ^0.3.2.
    - Downgraded flutter_polyline_points from ^3.0.1 to ^2.1.0.
    - Downgraded freezed from ^3.2.0 to ^3.0.6.
    - Downgraded freezed_annotation from ^3.1.0 to ^3.0.0.
    - Downgraded geocoding from ^4.0.0 to ^3.0.0.
    - Downgraded geolocator from ^14.0.2 to ^14.0.0.
    - Downgraded go_router from ^16.1.0 to ^15.1.1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:tager_paraffin/core/services/firebase_messaging_service.dart';
-import 'package:tager_paraffin/core/services/secure_storage_service.dart';
-import 'package:tager_paraffin/core/uitls/firebase_options.dart';
-import 'package:tager_paraffin/core/uitls/simple_observer.dart';
-import 'package:tager_paraffin/my_app.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-  // PlatformDispatcher.instance.onError = (error, stack) {
-  //   FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-  //   return true;
-  // };
-  await ScreenUtil.ensureScreenSize();
-  await NotificationsService.instance.initialize();
-  Bloc.observer = SimpleBlocObServer();
-  SecureStorageManager().init();
-  runApp(MyApp());
-}
-=======
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -41,15 +13,14 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-  // PlatformDispatcher.instance.onError = (error, stack) {
-  //   FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-  //   return true;
-  // };
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   await ScreenUtil.ensureScreenSize();
   await FirebaseNotificationService.instance.initialize();
   Bloc.observer = SimpleBlocObServer();
   SecureStorageManager().init();
   runApp(MyApp());
 }
->>>>>>> e1be5033add3d48e73b1409a31d21df9a80ea207
